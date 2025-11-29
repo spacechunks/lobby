241696e6b48a50eb1daec29f2345d899decac131
yannic rieger <ybr@76k.io>
2025-11-29 17:06:05 +0100

    agenerate protos

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
     kotlin("jvm") version "2.1.10"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("com.gradleup.shadow") version "9.0.0-beta4"
+    id("com.google.protobuf") version "0.9.5"
 }
 
 group = "space.chunks"
@@ -22,6 +24,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib"))
+    api("com.google.protobuf:protobuf-kotlin:3.23.1")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 }
 
@@ -57,5 +60,33 @@ tasks {
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
         minecraftVersion("1.21.4")
+    }
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDirs(
+                "build/generated/source/proto/main/kotlin",
+            )
+        }
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/java",
+            )
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.23.1"
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.builtins {
+                id("kotlin")
+            }
+        }
     }
 }
