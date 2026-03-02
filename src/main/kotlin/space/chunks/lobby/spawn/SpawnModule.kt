93f17b62d13926b0b1187afc87c37d6c5042195d
yannic rieger <ybr@76k.io>
2026-03-02 15:19:58 +0100

    provide logger instance per module (#26)
    also move chunk viewer config keys under chunkViewer

--- src/main/kotlin/space/chunks/lobby/spawn/SpawnModule.kt
+++ src/main/kotlin/space/chunks/lobby/spawn/SpawnModule.kt
@@ -8,7 +8,7 @@ import space.chunks.lobby.chunkviewer.display.DisplaySessionService
 class SpawnModule(
     private val sessSvc: DisplaySessionService,
     plugin: Plugin,
-) : LobbyModule(plugin) {
+) : LobbyModule(plugin, "spawn") {
     override fun onEnable() {
         val w = Bukkit.getWorld("devlobby")!!
 
