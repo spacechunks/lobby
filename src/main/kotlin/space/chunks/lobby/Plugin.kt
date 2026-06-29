df2d9e9c20487812a34bd007d154c1663d81c17d
yannic rieger <ybr@76k.io>
2026-06-29 18:01:27 +0200

    rewire modules in plugin main

--- src/main/kotlin/space/chunks/lobby/Plugin.kt
+++ src/main/kotlin/space/chunks/lobby/Plugin.kt
@@ -1,6 +1,11 @@
 package space.chunks.lobby
 
+import chunks.space.api.explorer.chunk.v1alpha1.ChunkServiceGrpcKt
+import chunks.space.api.explorer.instance.v1alpha1.InstanceServiceGrpcKt
+import chunks.space.api.matchmaking.v1alpha1.MatchmakingServiceGrpcKt
 import com.noxcrew.interfaces.InterfacesListeners
+import io.grpc.ManagedChannelBuilder
+import io.grpc.netty.NettyChannelBuilder
 import io.papermc.paper.event.connection.configuration.AsyncPlayerConnectionConfigureEvent
 import net.kyori.adventure.resource.ResourcePackInfo
 import net.kyori.adventure.resource.ResourcePackRequest
@@ -13,8 +18,12 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.generator.ChunkGenerator
 import org.bukkit.plugin.java.JavaPlugin
+import space.chunks.lobby.controlplane.instance.InstanceService
 import space.chunks.lobby.modules.chunkviewer.ChunkViewerModule
+import space.chunks.lobby.modules.chunkviewer.grpc.AuthCredentials
 import space.chunks.lobby.modules.chunkviewer.world.VoidWorldGenerator
+import space.chunks.lobby.modules.matchmaking.MMModule
+import space.chunks.lobby.modules.matchmaking.MMService
 import space.chunks.lobby.modules.party.PartyModule
 import space.chunks.lobby.modules.party.PartyService
 import space.chunks.lobby.modules.spawn.SpawnModule
@@ -28,28 +37,75 @@ import java.net.URI
 import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.seconds
+import space.chunks.lobby.controlplane.Config as ControlPlaneConfig
+import space.chunks.lobby.modules.matchmaking.Config as MMConfig
 
 class Plugin : JavaPlugin(), Listener {
+    private val cpConfig = ControlPlaneConfig.parse(this.config)
+    private val mmConfig = MMConfig.parse(this.config)
+
+    private val cpChannel = ManagedChannelBuilder
+        .forAddress(this.cpConfig.addr, this.cpConfig.port)
+        .keepAliveTime(30, TimeUnit.SECONDS)      // ping server every 30s
+        .keepAliveTimeout(10, TimeUnit.SECONDS)   // wait 10s for pong
+        .useTransportSecurity()
+        .build()
+
+    private val mmChannel = NettyChannelBuilder
+        .forAddress(this.mmConfig.addr, this.mmConfig.port)
+        .usePlaintext()
+        .keepAliveTime(30, TimeUnit.SECONDS)
+        .keepAliveTimeout(10, TimeUnit.SECONDS)
+        .keepAliveWithoutCalls(true)
+        .build()
+
+    private val mmClient = MatchmakingServiceGrpcKt.MatchmakingServiceCoroutineStub(mmChannel)
+    private val instanceClient = InstanceServiceGrpcKt.InstanceServiceCoroutineStub(cpChannel)
+        .withCallCredentials(AuthCredentials(this.cpConfig.apiToken))
+    private val chunkClient = ChunkServiceGrpcKt.ChunkServiceCoroutineStub(cpChannel)
+        .withCallCredentials(AuthCredentials(cpConfig.apiToken))
 
     private val packConfig = ResourcePackConfig.parse(this.config)
     private val packService = PackService(this.logger, this, packConfig)
     private val partyService = PartyService()
+    private val mmService = MMService(
+        this.logger,
+        this.mmClient,
+        this.mmConfig.ticketPollInterval.seconds,
+        this,
+    )
+    private val instanceService = InstanceService(
+        this.logger,
+        this.instanceClient,
+        this.cpConfig.instancePollIntervalSeconds.seconds,
+    )
     private val uiService = UiService(ActionBar::send)
     private val texts = Texts(this)
     private val bossbars = BossBars(this.uiService)
 
     // modules
-    private val chunkViewerMod = ChunkViewerModule(this, packConfig, partyService, texts, bossbars)
-    private val spawnMod = SpawnModule(this.chunkViewerMod.sessionService, this, texts, this.uiService)
-    private val partyMod = PartyModule(this, partyService, texts, this.bossbars)
+    private val chunkViewerMod = ChunkViewerModule(this, this.packConfig, this.chunkClient, this.texts)
+    private val spawnMod = SpawnModule(this.chunkViewerMod.sessionService, this, this.texts, this.uiService)
+    private val partyMod = PartyModule(this, this.partyService, this.texts, this.bossbars)
+    private val mmMod = MMModule(
+        this,
+        this.partyService,
+        this.mmService,
+        this.instanceService,
+        this.texts,
+        this.bossbars,
+        this.mmConfig,
+    )
+
+    private val modules = listOf(
+        chunkViewerMod,
+        spawnMod,
+        partyMod,
+        mmMod,
+    )
 
     override fun onEnable() {
-        val modules = listOf(
-            chunkViewerMod,
-            spawnMod,
-            partyMod,
-        )
-
         installInterfaces()
         Bukkit.getPluginManager().registerEvents(this.uiService, this)
         this.uiService.start(this)
@@ -58,6 +114,12 @@ class Plugin : JavaPlugin(), Listener {
 
         modules.forEach {
             it.onEnable()
+        }
+    }
+
+    override fun onDisable() {
+        this.modules.forEach {
+            it.onDisable()
         }
     }
 
